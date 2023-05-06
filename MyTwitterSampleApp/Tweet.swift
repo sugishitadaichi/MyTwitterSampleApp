5c47e046e3e96509d9fc936cf3aa9fdfd2d16439
sugishitadaichi <fusion.haa0743@outlook.jp>
2023-05-06 15:41:06 +0900

    ダミーデータ1行のみ作成

---
+++ MyTwitterSampleApp/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  MyTwitterSampleApp
+//
+//  Created by 杉下大智 on 2023/05/06.
+//
+
+import Foundation
+
+struct Tweet {
+    // ツイート本文
+    let text: String
+}
--- MyTwitterSampleApp/ViewController.swift
+++ MyTwitterSampleApp/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    //ツイートプロパティ
+    var tweetList: [Tweet] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
+        // setTweet関数をホーム画面が表示された際に反映させる
+        setTweet()
+    }
+    // ツイートを格納するためのメソッド
+    func setTweet() {
+        let tweetPost = Tweet(text: "あいうえお")
+        tweetList.append(tweetPost)
     }
 
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let tweet: Tweet = tweetList[indexPath.row]
+        cell.textLabel?.text = tweet.text
+        return cell
+    }
 }
 
