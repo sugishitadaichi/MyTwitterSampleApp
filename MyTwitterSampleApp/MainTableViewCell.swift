f835cd284246a781792759450120cbe5e4dacc7e
sugishitadaichi <fusion.haa0743@outlook.jp>
2023-05-07 22:05:27 +0900

    xibを使ったカスタムセル作成1

---
+++ MyTwitterSampleApp/MainTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainTableViewCell.swift
+//  MyTwitterSampleApp
+//
+//  Created by 杉下大智 on 2023/05/07.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
